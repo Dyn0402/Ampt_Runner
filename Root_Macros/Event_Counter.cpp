aa8e8bc4e29a2187441307e310c27bc32e42929d
Dyn0402 <Dyn0402@hotmail.com>
2020-04-10 17:02:59 -0700

    Set to run 250 jobs of 10000 7.7GeV events. Also added Event_Coutner.cpp

---
+++ Root_Macros/Event_Counter.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TPad.h"
+
+using namespace std;
+
+
+vector<string> get_files_in_dir(string, string, string);
+vector<string> split_string_by_char(string, char);
+
+
+int Event_Counter() {
+	string path = "/home/dylan/Research/AMPT_Test_Data/7GeV/";
+	vector<string> in_files = get_files_in_dir(path, "root", "path");
+	TH1I *num_events = new TH1I("Num_Events", "Number of AMPT Events", 101, -0.5, 1000.5);
+	int num_quit = 0;
+	for(string file:in_files) {
+		TFile *f = new TFile(file.data(), "READ");
+		TTree *tree = (TTree*)f->Get("tree");
+		num_events->Fill(tree->GetEntries());
+		if(tree->GetEntries() < 1000) { cout << "Entries: " << tree->GetEntries() << endl; num_quit++; }
+		f->Close();
+		delete f;
+	}
+	cout << endl << "Number died early: " << num_quit << endl;
+	num_events->Draw();
+	return 0;
+}
+
+
+vector<string> split_string_by_char(string str, char del) {
+	vector<string> split;
+	stringstream stream(str);
+	string hold;
+	while(getline(stream, hold, del)) {
+		split.push_back(hold);
+	}
+
+	return(split);
+}
+
+
+//Return name (not path) of all files in dir_path with extension ext.
+vector<string> get_files_in_dir(string dir_path, string ext, string out) {
+	vector<string> files;
+	struct stat info;
+
+	if( stat(dir_path.data(), &info) !=0 ) {
+		cout << "Can't access path: " << dir_path << " Skipping following read/write." << endl;
+	} else {
+		DIR* files_dir = opendir(dir_path.data());
+		struct dirent* dp;
+		while((dp=readdir(files_dir)) != NULL) {
+			string file = dp->d_name;
+			vector<string> fields = split_string_by_char(file, '.');
+			if(fields.back() == ext) {
+				files.push_back(file);
+			}
+		}
+		if(out == "path") {
+			for(unsigned i=0; i<files.size(); i++) {
+				files[i] = dir_path + files[i];
+			}
+		}
+	}
+
+	return(files);
+}
