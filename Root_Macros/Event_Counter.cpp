c84d408574ea11134f02134affd6d0ce9a9a7198
Dyn0402 <Dyn0402@hotmail.com>
2020-04-12 00:14:38 -0700

    Apparent typo on 19GeV, set to run again.

--- Root_Macros/Event_Counter.cpp
+++ Root_Macros/Event_Counter.cpp
@@ -20,19 +20,23 @@ vector<string> split_string_by_char(string, char);
 
 
 int Event_Counter() {
-	string path = "/home/dylan/Research/AMPT_Test_Data/7GeV/";
+	int target_events = 2000;
+	string path = "/media/dylan/SSD_Storage/Research/Trees_Ampt/19GeV/";
+
 	vector<string> in_files = get_files_in_dir(path, "root", "path");
-	TH1I *num_events = new TH1I("Num_Events", "Number of AMPT Events", 101, -0.5, 1000.5);
-	int num_quit = 0;
+	TH1I *num_events = new TH1I("Num_Events", "Number of AMPT Events", target_events / 100 + 1, -0.5, target_events + 0.5);
+	int num_quit = 0, total_events = 0;
 	for(string file:in_files) {
 		TFile *f = new TFile(file.data(), "READ");
 		TTree *tree = (TTree*)f->Get("tree");
 		num_events->Fill(tree->GetEntries());
-		if(tree->GetEntries() < 1000) { cout << "Entries: " << tree->GetEntries() << endl; num_quit++; }
+		total_events += tree->GetEntries();
+		if(tree->GetEntries() < target_events) { cout << "Entries: " << tree->GetEntries() << endl; num_quit++; }
 		f->Close();
 		delete f;
 	}
 	cout << endl << "Number died early: " << num_quit << endl;
+	cout << "Total number of events: " << total_events << endl;
 	num_events->Draw();
 	return 0;
 }
