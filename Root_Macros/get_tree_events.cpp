7dbd3e66a1c4103337a1431dea6b83deb1b66fd9
Dyn0402 <Dyn0402@hotmail.com>
2020-04-20 19:55:50 -0700

    Edited get_tree_events.cpp to read all trees in directory. Else too slow

--- Root_Macros/get_tree_events.cpp
+++ Root_Macros/get_tree_events.cpp
@@ -1,18 +1,68 @@
 
 #include <iostream>
+#include <dirent.h>
+
 
 #include "TFile.h"
 #include "TTree.h"
 
 using namespace std;
 
+
+vector<string> get_files_in_dir(string, string, string);
+vector<string> split_string_by_char(string, char);
+
+
 int get_tree_events(string path) {
 
-	TFile *file = new TFile(path.data(), "READ");
-	TTree *tree = (TTree*)file->Get("tree");
-	cout << "Number of events in tree: " << tree->GetEntries() << endl;
-	file->Close();
-	delete file;
+	vector<string> in_files = get_files_in_dir(path, "root", "path");
+	for(string file:in_files) {
+		TFile *file = new TFile(path.data(), "READ");
+		TTree *tree = (TTree*)file->Get("tree");
+		cout << "Number of events in tree: " << file << " " << tree->GetEntries() << endl;
+		file->Close();
+		delete file;
+	}
 
 	return 0;
+}
+
+
+vector<string> split_string_by_char(string str, char del) {
+	vector<string> split;
+	stringstream stream(str);
+	string hold;
+	while(getline(stream, hold, del)) {
+		split.push_back(hold);
+	}
+
+	return(split);
+}
+
+
+//Return name (not path) of all files in dir_path with extension ext.
+vector<string> get_files_in_dir(string dir_path, string ext, string out) {
+	vector<string> files;
+	struct stat info;
+
+	if( stat(dir_path.data(), &info) !=0 ) {
+		cout << "Can't access path: " << dir_path << " Skipping following read/write." << endl;
+	} else {
+		DIR* files_dir = opendir(dir_path.data());
+		struct dirent* dp;
+		while((dp=readdir(files_dir)) != NULL) {
+			string file = dp->d_name;
+			vector<string> fields = split_string_by_char(file, '.');
+			if(fields.back() == ext) {
+				files.push_back(file);
+			}
+		}
+		if(out == "path") {
+			for(unsigned i=0; i<files.size(); i++) {
+				files[i] = dir_path + files[i];
+			}
+		}
+	}
+
+	return(files);
 }
